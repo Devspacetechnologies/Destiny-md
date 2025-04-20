a1c255956662cb40db203e22b43268606ef2eca8
Mark <153409985+Devspacetechnologies@users.noreply.github.com>
2025-04-20 12:47:54 +0300

    Update yt-stalk.js

--- plugins/yt-stalk.js
+++ plugins/yt-stalk.js
@@ -32,7 +32,7 @@ cmd({
       + `┃🎥 *Videos:* ${yt.video_count}\n`
       + `┃🔗 *Channel Link:* (${yt.channel})\n`
       + `╰━━━⪼\n\n`
-      + `🔹 *Powered by pkdriller*`;
+      + `🔹 *Powered by 𝙼𝚊𝚛𝚔*`;
 
     await conn.sendMessage(from, {
       image: { url: yt.avatar },
