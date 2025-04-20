051584ab18ab22e6f93121543d89146d8b19063d
Mark <153409985+Devspacetechnologies@users.noreply.github.com>
2025-04-20 12:31:34 +0300

    Update check-update.js

--- plugins/check-update.js
+++ plugins/check-update.js
@@ -66,7 +66,7 @@ Use *.update* to update.`;
     }
 
     const statusMessage = `*Good ${new Date().getHours() < 12 ? 'Morning' : 'Night'}, ${pushname}!* 🌟\n\n` +
-      `📌 *Bot Name:* DESTINY MFD\n🔖 *Current Version:* ${localVersion}\n📢 *Latest Version:* ${latestVersion}\n📂 *Total Plugins:* ${pluginCount}\n🔢 *Total Commands:* ${totalCommands}\n\n` +
+      `📌 *Bot Name:* DESTINY MD\n🔖 *Current Version:* ${localVersion}\n📢 *Latest Version:* ${latestVersion}\n📂 *Total Plugins:* ${pluginCount}\n🔢 *Total Commands:* ${totalCommands}\n\n` +
       `💾 *System Info:*\n⏳ *Uptime:* ${uptime}\n📟 *RAM Usage:* ${ramUsage}MB / ${totalRam}MB\n⚙️ *Host Name:* ${hostName}\n📅 *Last Update:* ${lastUpdate}\n\n` +
       `📝 *Changelog:*\n${latestChangelog}\n\n` +
       `⭐ *GitHub Repo:* ${githubRepo}\n👤 *Owner:* [pkdriller](https://github.com/pkdriller)\n\n${updateMessage}\n\n🚀 *Hey! Don't forget to fork & star the repo!*`;
